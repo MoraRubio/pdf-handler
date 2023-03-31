af4dac6402c4b0c4b6bd770be1701b225206f7e2
Alejandro Mora Rubio <morarubioa@gmail.com>
2023-03-31 08:52:04 -0500

    Update to PyQt6, disable pikepdf

--- UI.py
+++ UI.py
@@ -1,13 +1,16 @@
-from main import *
+#from main import *
 from pdf2image import convert_from_path
 from numpy import ceil
 from os import getcwd
 
 from window_ui import *
-from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import *
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtWidgets import *
 from PIL.ImageQt import ImageQt, toqpixmap
+
+def get_filename(path):
+    return path.split('/')[-1].split('.')[0]
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
@@ -26,9 +29,9 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
         self.msg = QMessageBox()
         self.msg.setWindowTitle('Confirmar orden seleccionado')
-        self.msg.setIcon(QMessageBox.Question)
-        self.msg.setStandardButtons(QMessageBox.Ok|QMessageBox.Retry)
-        self.msg.setDefaultButton(QMessageBox.Ok)
+        self.msg.setIcon(QMessageBox.Icon.Question)
+        self.msg.setStandardButtons(QMessageBox.StandardButton.Ok|QMessageBox.StandardButton.Retry)
+        self.msg.setDefaultButton(QMessageBox.StandardButton.Ok)
     
     def clear_files(self):
         self.listWidget_files.clear()
@@ -43,28 +46,29 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.label_3.setText(self.destination)
 
     def merge_files(self):
-        ordered_paths = []
-        for filename in [self.listWidget_files.item(x).text() for x in range(self.listWidget_files.count())]:
-            ordered_paths.append(next(path for path in self.paths if filename in path))
-        merge(ordered_paths, self.destination)
-        self.paths = ordered_paths.copy()
+        # ordered_paths = []
+        # for filename in [self.listWidget_files.item(x).text() for x in range(self.listWidget_files.count())]:
+        #     ordered_paths.append(next(path for path in self.paths if filename in path))
+        # merge(ordered_paths, self.destination)
+        # self.paths = ordered_paths.copy()
         self.msg_completed()
 
     def split_files(self):
-        for path in self.paths:
-            split(path, self.destination)
+        # for path in self.paths:
+        #     split(path, self.destination)
         self.msg_completed()
 
     def reorganize_file(self):
-        self.msg.setText('El orden seleccionado es: '+', '.join([item.text() for item in self.listWidget.selectedItems()]))
-        x = self.msg.exec_()
-        if str(x) == '1024':
-            reorganize(self.paths[self.listWidget_files.currentRow()],
-                       [int(item.text()[-1]) for item in self.listWidget.selectedItems()],
-                       self.destination)
-            self.msg_completed()
-        else:
-            pass
+        # self.msg.setText('El orden seleccionado es: '+', '.join([item.text() for item in self.listWidget.selectedItems()]))
+        # x = self.msg.exec_()
+        # if str(x) == '1024':
+        #     reorganize(self.paths[self.listWidget_files.currentRow()],
+        #                [int(item.text()[-1]) for item in self.listWidget.selectedItems()],
+        #                self.destination)
+        #     self.msg_completed()
+        # else:
+        #     pass
+        self.msg_completed()
     
     def msg_completed(self):
         msg_completed = QMessageBox()
@@ -96,13 +100,13 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         for n, image in enumerate(images):
             img_label = QLabel()
             text_label = QLabel()
-            img_label.setAlignment(Qt.AlignCenter)
-            text_label.setAlignment(Qt.AlignCenter)
+            img_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+            text_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
             img_label.setMargin(0)
             text_label.setMargin(0)
             qim = ImageQt(image)
             pixmap = QPixmap.fromImage(qim)
-            pixmap = pixmap.scaled(QSize(1500, 300), 1, 1)
+            pixmap = pixmap.scaled(1500, 300, Qt.AspectRatioMode.KeepAspectRatio)
             img_label.setPixmap(pixmap)
             text_label.setText(f'Página {str(n+1)}')
             thumbnail = QVBoxLayout()
@@ -124,4 +128,4 @@ if __name__ == "__main__":
     app = QtWidgets.QApplication([])
     window = MainWindow()
     window.show()
-    app.exec_()
+    app.exec()
